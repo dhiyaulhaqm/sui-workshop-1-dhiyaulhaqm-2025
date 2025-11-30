5ac929d02f501de881ba2dcd06730c6238f8c46a
dhiyaulhaqm <142429841+dhiyaulhaqm@users.noreply.github.com>
2025-11-30 21:11:02 +0800

    workshop done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
 "use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { WalletConnection } from "@/components/WalletConnection";
+import { Wallet } from "lucide-react";
+import Balance from "@/components/Balance";
+import Faucet from "@/components/transaction/Faucet";
+import Escrow from "@/components/transaction/Escrow";
+import ListMyEscrow from "@/components/transaction/ListMyEscrow";
+
 
 export default function page() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle className="text-4xl font-bold">Workshop SUI - Frontend</CardTitle>
+        <CardTitle className="text-4xl font-bold">
+          Workshop SUI - Frontend
+        </CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
         <span>Start Here!</span>
+        <WalletConnection />
+        <Balance />
+        <Faucet/>
+        <Escrow/>
+        <ListMyEscrow/>
+
+
       </CardContent>
     </Card>
-  )
-};
+  );
+}
